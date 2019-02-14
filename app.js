dba3dcbbed3ded3a3320be7e84f7739961a603df
Lee-Roy King <leeroywking@gmail.com>
2019-02-13 20:19:36 -0800

    skeleton files

---
+++ app.js
@@ -0,0 +1,28 @@
+'user strict';
+
+var days= [
+'Monday',
+'Tuesday',
+'Wednesday',
+'Thursday',
+'Friday',
+'Saturday',
+'Sunday',
+]
+
+var alkiUl = document.getElementById('alkibeach');
+
+var aliki = {
+    name: 'Alki',
+    neighborhood: 'West Seattle',
+    seagullCount: [
+        212,
+        44,
+        34,
+        14,
+        15,
+        23,
+        42,
+    ],
+    
+}
